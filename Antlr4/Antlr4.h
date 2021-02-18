f8be0e8faf1d9b252acdbbcbe2e2f94ec301b4da
BAN Jun <banjun@gmail.com>
2021-02-18 21:26:04 +0900

    parse by Antlr parser

---
+++ Antlr4/Antlr4.h
@@ -0,0 +1,18 @@
+//
+//  Antlr4.h
+//  Antlr4
+//
+//  Created by BAN Jun on 2021/02/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Antlr4.
+FOUNDATION_EXPORT double Antlr4VersionNumber;
+
+//! Project version string for Antlr4.
+FOUNDATION_EXPORT const unsigned char Antlr4VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Antlr4/PublicHeader.h>
+
+
--- SparqlSandbox/ViewController.swift
+++ SparqlSandbox/ViewController.swift
@@ -1,26 +1,24 @@
-//
-//  ViewController.swift
-//  SparqlSandbox
-//
-//  Created by BAN Jun on 2021/02/18.
-//
-
 import Cocoa
+import Antlr4
 
 class ViewController: NSViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        let input = ANTLRInputStream("""
+PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+PREFIX dbpprop: <http://dbpedia.org/property/>
+PREFIX dbres: <http://dbpedia.org/resource/>
 
-        // Do any additional setup after loading the view.
+SELECT ?y WHERE {
+ ?y dbpedia-owl:binomialAuthority dbres:Johan_Christian_Fabricius.
+ }
+""")
+        let lexer = SparqlLexer(input)
+        let tokens = CommonTokenStream(lexer)
+        let parser = try! SparqlParser(tokens)
+
+        let query = try! parser.query()
+        print(query.toStringTree(parser))
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
 
